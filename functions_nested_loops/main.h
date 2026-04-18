de315eed3944a96c9c9004ad37169fd6a330bf97
rukshar06 <saibrukshar@gmail.com>
2026-04-18 20:08:27 +0000

    Fix: remove incorrect 0-putchar.c directory

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,25 +1,6 @@
-
-
-/**
- * print_alphabet_x10 - prints the alphabet 10 times
- */
-void print_alphabet_x10(void)
-{
-	int i, c;
-
-	for (i = 0; i < 10; i++)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
 #ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
 
-#endif /* MAIN_H */	}
-}
+#endif
