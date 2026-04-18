7f70c4be48f732c0c248b1f884e6edc1a49818d2
rukshar06 <saibrukshar@gmail.com>
2026-04-18 18:52:37 +0000

    task 2

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,21 @@
-#ifndef MAIN_H
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i, c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}#ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
