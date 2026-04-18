9d6a6fc078633023d5c29b8a25bae943b2bf40b1
rukshar06 <saibrukshar@gmail.com>
2026-04-18 19:22:37 +0000

    task 2

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -14,14 +14,6 @@ void print_alphabet_x10(void)
 			_putchar(c);
 		}
 		_putchar('\n');
-	}
-}#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-
-#endif
-void print_alphabet(void);
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -29,21 +21,5 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 
-#endif
-
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-
-#endif
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-
-#endif
+#endif /* MAIN_H */	}
+}
