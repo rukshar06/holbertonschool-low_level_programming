f6b60d1f65c7d84c31fdd967a469f426727ecef6
rukshar06 <saibrukshar@gmail.com>
2026-04-25 19:48:36 +0000

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+/**
+ * _puts - prints a string of a characters
+ * @str: is the string to be printed
+ *
+ * Return: the string
+ */
+void _puts(char *str)
+{
+	char[] = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+
+	printf("%s\n", char);
+	return (0);
+}
