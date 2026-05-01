95221722c3b747af00cefbcfa1d6c4ebb54bbfdd
rukshar06 <saibrukshar@gmail.com>
2026-05-02 00:50:51 +0400

    task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,11 +22,11 @@ printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 if (last_digit == 0)
 {
-printf("Last digit of % is %d and is 0\n", n, last_digit);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 if (last_digit < 6 && last_digit != 0)
 {
-printf("Last digit of % is %d and is less than 6 but not 0\n", n, last_digit);
+printf("Last digit of %d is %d and is less than 6 but not 0\n", n, last_digit);
 }
 	return (0);
 }
