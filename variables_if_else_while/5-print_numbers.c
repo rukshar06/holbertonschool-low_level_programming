02b3dab2ef329c3c5eaa51174dbb47d0391060f1
rukshar06 <saibrukshar@gmail.com>
2026-05-03 22:24:49 +0400

    task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints all single digit numbers
+*
+*Return: Always 0
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+printf("%d", i);
+
+printf("\n");
+
+return (0);
+}
