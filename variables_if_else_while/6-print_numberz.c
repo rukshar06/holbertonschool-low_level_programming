c74aee12261b34b2ef3df1174e93a2a10e51babf
rukshar06 <saibrukshar@gmail.com>
2026-05-03 22:33:28 +0400

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints all digit
+*
+*Return: Always 0
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+putchar(i);
+
+putchar('\n');
+
+return (0);
+}
