5ac78b9d8f4b805e1e47cd9a891e906c49c4d0fa
rukshar06 <saibrukshar@gmail.com>
2026-05-06 08:37:17 +0400

    task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,19 +1,26 @@
 #include "main.h"
 /**
- * print_square - prints a square followed by a new line
- * @param - size of the square
+ * print_square - prints a square using #
+ * @size: size of the square
+ *
+ * Return: void
  */
 void print_square(int size)
 {
-	if (size < 1)
-		return;
-
 	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
+		{
 			_putchar('#');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
