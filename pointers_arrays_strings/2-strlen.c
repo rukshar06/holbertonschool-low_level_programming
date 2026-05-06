f826325f9fa86b319ed5899a5a95ed51f00a5066
rukshar06 <saibrukshar@gmail.com>
2026-05-06 13:32:34 +0400

    task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: check string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != 0)
+	{
+		len++;
+	}
+
+	return (len);
+}
