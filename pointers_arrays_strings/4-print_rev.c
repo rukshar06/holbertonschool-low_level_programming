2731dba241e4584903b12012bf345d82c8594050
rukshar06 <saibrukshar@gmail.com>
2026-05-07 11:07:49 +0400

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: check the string
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len--;
+
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+
+	_putchar ('\n');
+}
