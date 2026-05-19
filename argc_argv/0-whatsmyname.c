4605372f33a4fed620e76b132842ff706beedd07
rukshar06 <saibrukshar@gmail.com>
2026-05-19 09:42:52 +0400

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - prints the program name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	(argc --);
+
+	while (argv[0][i] != '\0')
+	{
+		_putchar(argv[0][i]);
+		i++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
