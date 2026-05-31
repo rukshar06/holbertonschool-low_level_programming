832d16723868ad989f2c8bb5bcf1b17c4efdea4d
rukshar06 <saibrukshar@gmail.com>
2026-05-31 23:28:09 +0400

    task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,33 +9,31 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2;
-    int (*func)(int, int);
+	int num1, num2;
+	int (*func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    func = get_op_func(argv[2]);
+	func = get_op_func(argv[2]);
 
-    if (func == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-
-    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-
-    printf("%d\n", func(num1, num2));
-
-    return (0);
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", func(num1, num2));
+	return (0);
 }
