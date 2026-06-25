74b511caec0f80fd103ee0722339eb03f12d69a8
rukshar06 <saibrukshar@gmail.com>
2026-06-25 22:11:36 +0400

    task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of the file
+ * @text_content: NULL terminated string to write
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0, written;
+
+	if (filename == NULL)
+		return (-1);
+
+	while (text_content && text_content[len])
+		len++;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (len > 0)
+	{
+		written = write(fd, text_content, len);
+		if (written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
